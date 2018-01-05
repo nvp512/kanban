e89c08e07999f867147787accdd802e40a79fb88
phunv <nguyenvinhphu1111@gmail.com>
2018-01-05 19:49:07 +0900

    drag

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ var app = angular.module('myApp', ['ngAnimate', 'ngSanitize', 'dndLists'])
 
 // use service
 app.controller('kanBanCtrl', function($scope){
+  console.log('Hello World!');
+
   var ctrl = this;
   ctrl.taskList = [
     {tasks: [{name: 'Task 1', selected: false}, {name: 'Task 2', selected: false}, {name: 'Task 3', selected: false}], label: 'TO DO', class: 'info', dragging: false},
@@ -10,5 +12,31 @@ app.controller('kanBanCtrl', function($scope){
     {tasks: [{name: 'Task 7', selected: false}, {name: 'Task 8', selected: false}, {name: 'Task 9', selected: false}], label: 'PENDING', class: 'warning', dragging: false},
     {tasks: [{name: 'Task 10', selected: false}, {name: 'Task 13', selected: false}, {name: 'Task 12', selected: false}], label: 'DONE', class: 'success', dragging: false}
   ]
-  console.log('Hello World!');
+    // Hàm này sẽ xử lý việc gán trường *selected* của phần tử đang được dragged bằng true, và lấy các phần tử khác đang được select (selected = true) để thực hiện drop.
+  ctrl.getSelectedTasksIncluding = function(list, task) {
+      task.selected = true;
+      return _.filter(list.tasks, ['selected', true]);
+    };
+
+  // Hàm này sẽ gán trường *dragging* của list có phần tử đang được dragged bằng true.
+    ctrl.onDragstart = function(list, event) {
+      list.dragging = true;
+    };
+
+  // Hàm này sẽ gán trường *selected* của tất cả các phần tử trong mảng thành false
+  // Thường thì khi chúng ta drag, các phần tử được dragged sẽ có trường *selected* bằng true, vì thế sau khi drop, chúng ta phải gán lại trường *selected* của các phần tử đó bằng false.
+  // Sau đó chúng ta sẽ thực hiện việc thêm các phần tử được dragged đó vào list mới.
+  // Ở đây index chính là vị trí mà chúng ta drop vào trong mảng, vì vậy để chèn các phần tử đó vào chúng ta sẽ thực hiện như bên dưới.
+    ctrl.onDrop = function(list, tasks, index) {
+      _.forEach(tasks, function(task) { task.selected = false; });
+      list.tasks = list.tasks.slice(0, index).concat(tasks).concat(list.tasks.slice(index));
+      return true;
+    }
+
+  // Như mình đã giải thích ở trên, hàm này sẽ có tác dụng loại bỏ phần tử đang được dragged, bằng cách loại bỏ những phần tử có trường *selected* bằng true.
+    ctrl.onMoved = function(list) {
+      list.tasks = _.filter(list.tasks, ['selected', false]);
+    };
+
+
 });
